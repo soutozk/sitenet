740b540734c56038c90be22db34f110ea5ba0159
Gabriel Souto <154080694+soutozk@users.noreply.github.com>
2025-08-05 20:46:05 -0300

    feat: Integrate NavBar component into App layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="overflow-hidden">
+      <NavBar />
       <Outlet />
     </div>
   );
